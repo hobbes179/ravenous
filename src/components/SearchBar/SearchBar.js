614dc2579e70a63d7523c1c53ab0937c323e3ff7
Frank Hoey <hobbes179@gmail.com>
2018-04-16 23:30:44 -0500

    first lesson complete

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions {
+const sortByOptions = {
   'Best Match' : 'best_match',
   'Highest Rated' : 'rating',
   'Most Reviewed' : 'review_count'
@@ -13,25 +13,26 @@ class SearchBar extends React.Component {
         let sortByOptionValue = sortByOptions[sortByOption];
         return <li key={sortByOptionValue}>{sortByOption}</li>;
       });
-
-      render() {
-        return (
-          <div className="SearchBar">
-            <div className="SearchBar-sort-options">
-              <ul>
-                <!-- Use .renderSortByOptions() to sort the businesses by their options -->
-              </ul>
-            </div>
-            <div className="SearchBar-fields">
-              <input placeholder="Search Businesses" />
-              <input placeholder="Where?" />
-            </div>
-            <div className="SearchBar-submit">
-              <a>Let's Go</a>
-            </div>
-          </div>
-        )
-      }
-
     }
+
+    render() {
+      return (
+        <div className="SearchBar">
+          <div className="SearchBar-sort-options">
+            <ul>
+              {this.renderSortByOptions()}
+            </ul>
+          </div>
+          <div className="SearchBar-fields">
+            <input placeholder="Search Businesses" />
+            <input placeholder="Where?" />
+          </div>
+          <div className="SearchBar-submit">
+            <a>Let's Go</a>
+          </div>
+        </div>
+      )
+    };
 }
+
+export default SearchBar;
